cea45061d050ae96f587b5f3989ee31e6e460185
daehyunkim <keogus2000@gmail.com>
2026-03-04 14:31:34 +0900

    Update import_requests.py

--- import_requests.py
+++ import_requests.py
@@ -1,11 +1,11 @@
 import requests
 
 # 1. 아까 받은 토큰 (예: 7123...:AAF...)
-token = "7947719889:AAHZy-86EFoh5H7sWRIGjae6kEnFNIjBpEg"
+token = "-"
 
 # 2. 숫자 ID (예: 6812...) 
 # 주의: @아이디 아닙니다! 무조건 숫자여야 해요.
-chat_id = "8077219703"
+chat_id = "-"
 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 params = {"chat_id": chat_id, "text": "테스트 메시지입니다!"}
